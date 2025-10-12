08237f130419a8cd02613901fe2522cb64184afe
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-10-12 17:56:11 +0100

    Remove unused PrintWriter import from Recipient servlet

--- src/main/java/com/example/bbm/Servlets/Recipient.java
+++ src/main/java/com/example/bbm/Servlets/Recipient.java
@@ -4,7 +4,6 @@ import com.example.bbm.dao.RecipientDAO;
 import com.example.bbm.dto.RecipientDTO;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,18 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Recipient extends HttpServlet {
     private RecipientDAO recipientDAO;
-    private RecipientDTO recipientDTO;
-public void init() {
-    this.recipientDAO = new RecipientDAO();
-    this.recipientDTO = new RecipientDTO();
-}
+
+    public void init() {
+        this.recipientDAO = new RecipientDAO();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        RequestDispatcher dispatch  = request.getRequestDispatcher("pages/recipient.jsp");
+        RequestDispatcher dispatch = request.getRequestDispatcher("pages/recipient.jsp");
         List<RecipientDTO> recipientDTO = recipientDAO.findAll();
         request.setAttribute("recipients", recipientDTO);
         dispatch.forward(request, response);
     }
-public void destroy(){}
+    public void destroy() {
+    }
 }
